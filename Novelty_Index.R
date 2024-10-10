654ac9586d451266609cec5b66e198a89f269081
dvenegas <douglas.venegasg@gmail.com>
2024-10-09 20:40:40 -0600

    Script_update

--- Novelty_Index.R
+++ Novelty_Index.R
@@ -10,6 +10,7 @@ library(rJava)
 library(rcdk)
 library(RColorBrewer)
 library(writexl)
+library(httr)
 
 # Load network from a GraphML file. Change name
 
@@ -326,7 +327,7 @@ IQ_Cluster_plot= ggplot(IQ_w_metadata, aes(x = Cluster, y = IQ, color = ATTRIBUT
 ggsave("Data/IQ_Plot/1_IQ_Cluster_plot.png", plot = IQ_Cluster_plot, width = 15, height = 8)
 
 
-# Summary top 10 lower SI
+# Summary top 10 highest SI
 
 IQ_Summary_top <- IQ_Summary %>%
   arrange(desc(IQ)) %>%
@@ -337,7 +338,7 @@ p_summary <- ggplot(IQ_Summary_top, aes(x = Sample)) +
   geom_point(aes(y = Hmsq, color = "Hmsq"), size = 3) +
   geom_point(aes(y = Hcos, color = "Hcos"), size = 3, shape = 17) +
   geom_point(aes(y = Ans, color = "Ans"), size = 3, shape = 15) +
-  labs(title = "Top 10 Samples with the Lowest IQ parameters",
+  labs(title = "Top 10 Samples with the highest IQ parameters",
        x = "Sample",
        y = "Value",
        color = "Parameters") +
@@ -357,7 +358,7 @@ ggsave("Data/IQ_Plot/2_Parameters_summary.png", plot = p_summary, width = 15, he
 # IQ Summary plot
 p_summary <- ggplot(IQ_Summary_top, aes(x = Sample)) +
   geom_point(aes(y = IQ, color = "IQ"), size = 3) +
-  labs(title = "Top 10 Samples with the Lowest IQ",
+  labs(title = "Top 10 Samples with the Highest IQ",
        x = "Sample",
        y = "IQ") +
   theme(axis.text.x = element_text(angle = -45, hjust = 1, vjust = 1, size = 8),
@@ -480,7 +481,7 @@ IQ_by_strain_Summary_top <- IQ_by_strain %>%
 # IQ_by_strain Summary plot
 IQ_by_strain_Summary_top_plot <- ggplot(IQ_by_strain_Summary_top, aes(x = ATTRIBUTE_strain)) +
   geom_point(aes(y = total_IQ, color = "IQ"), size = 3) +
-  labs(title = "Top 10 Samples with the Lowest IQ by strain",
+  labs(title = "Top 10 Samples with the highest IQ by strain",
        x = "Sample",
        y = "IQ") +
   theme(axis.text.x = element_text(angle = -45, hjust = 1, vjust = 1, size = 8),
@@ -494,7 +495,7 @@ IQ_by_strain_Summary_top_plot <- ggplot(IQ_by_strain_Summary_top, aes(x = ATTRIB
 
 IQ_by_strain_Summary_top_plot
 
-ggsave("Data/IQ_Plot/7_IQ_by_strain_Plot_summary.png", plot = p_summary, width = 15, height = 8)
+ggsave("Data/IQ_Plot/7_IQ_by_strain_Plot_summary.png", plot = IQ_by_strain_Summary_top_plot , width = 15, height = 8)
 
 # BiNI integration
 # BiNi result
@@ -504,7 +505,6 @@ BiNI_results <- read_excel("Data/BiNI_results.xlsx")
 IQ_BiNI_by_strain <- merge(IQ_by_strain, BiNI_results, by.x = "ATTRIBUTE_strain", by.y = "Strain")
 
 # IQ-BiNi plot
-
 
 IQ_BiNI_by_strain_plot = ggplot(IQ_BiNI_by_strain, aes(x = ATTRIBUTE_strain)) +
   geom_point(aes(y = total_IQ, shape = "IQ", color = ATTRIBUTE_strain), size = 3) +    
@@ -520,99 +520,270 @@ IQ_BiNI_by_strain_plot = ggplot(IQ_BiNI_by_strain, aes(x = ATTRIBUTE_strain)) +
 
 IQ_BiNI_by_strain_plot
 
-ggsave("Data/IQ_Plot/4_IQ_BiNI_Plot.png", plot = IQ_BiNI_Plot, width = 15, height = 8)
-write_xlsx(IQ_BiNI, path = "Data/CSV_IQ/2_IQ_BINI.xlsx")
+ggsave("Data/IQ_Plot/7_IQ_by_strain_BiNI_Plot.png", plot = IQ_BiNI_by_strain_plot, width = 15, height = 8)
+write_xlsx(IQ_BiNI_by_strain, path = "Data/CSV_IQ/4_IQ_BINI_by_strain.xlsx")
+
+
+# NPAtlas integration
+
+NPAtlas_data <- read_excel("Data/NPAtlas_data.xlsx")
+NPAtlas_data= NPAtlas_data %>% select(compound_name, compound_inchi, compound_smiles, origin_type, genus, origin_species)
+
+#Extract index inputs
+score_MSQ <- df_nodes %>%
+  select(id, MQScore, UniqueFileSources, Smiles, INCHI, Compound_Name) %>%
+  filter(!is.na(MQScore) & MQScore != "") # Extract MSQ and remove NAs
+
+# canonizise smiles
+
+NPAtlas_data$Canonical_Smiles <- sapply(NPAtlas_data$compound_smiles, function(smiles) {
+  mol <- parse.smiles(smiles)[[1]]
+  get.smiles(mol)
+})
+NPAtlas_data= NPAtlas_data %>% select(-compound_smiles)
+
+score_MSQ$Canonical_Smiles= sapply(score_MSQ$Smiles, function(smiles){
+  mol <- parse.smiles(smiles)
+  if (is.null(mol[[1]])) {
+    return(NA)  
+  } else {
+    return(get.smiles(mol[[1]]))
+  }
+})
+score_MSQ= score_MSQ %>% select(-Smiles)
+
+# get NPAtlas anotations
+
+#By smiles
+MQS_score_NPAtlas_smiles <- left_join(score_MSQ, NPAtlas_data, by = "Canonical_Smiles", 
+                                      relationship = "many-to-many")%>% filter(!is.na(origin_type))
+# By inchi
+MQS_score_NPAtlas_inchi <- left_join(score_MSQ, NPAtlas_data, 
+                       by = c("INCHI" = "compound_inchi"), 
+                       relationship = "many-to-many") %>%
+  filter(!is.na(origin_type))
+
+#By name
+MQS_score_NPAtlas_name <- left_join(score_MSQ, NPAtlas_data, 
+                                     by = c("Compound_Name" = "compound_name"), 
+                                     relationship = "many-to-many") %>%
+  filter(!is.na(origin_type))
+
+# COMBINE
+MQS_score_NPAtlas <- bind_rows(MQS_score_NPAtlas_smiles, MQS_score_NPAtlas_inchi, MQS_score_NPAtlas_name) %>% 
+  distinct(id, .keep_all = TRUE) %>%
+  select(id, MQScore, UniqueFileSources)
+
+#Cos_score
+score_COS_node1_NPAtlas <- df_edges %>%
+  select(node1, cosine_score, node2) %>%
+  semi_join(MQS_score_NPAtlas, by = c("node2" = "id")) #Cosine values inputs from node to node
+score_COS_node2_NPAtlas <- df_edges %>%
+  select(node2, cosine_score, node1) %>%
+  semi_join(MQS_score_NPAtlas, by = c("node1" = "id")) #Complementary cosine values inputs
+
+# Obtain Cos value
+combined_interactions_NPAtlas <- bind_rows(
+  score_COS_node1_NPAtlas %>% mutate(direction = "Score_COS_node1"),
+  score_COS_node2_NPAtlas %>% mutate(direction = "Score_COS_node2")
+) #Add a new column called direction to indicate the direction of the interaction by referencing the node of interest.
+
+combined_interactions_NPAtlas <- combined_interactions_NPAtlas %>% mutate(
+  combined_nodes_NPAtlas = ifelse(direction == "Score_COS_node1",
+                          paste(node1, node2, sep = "/"),
+                          paste(node2, node1, sep = "/"))
+) #Verification of the value of the direction column. If “Score_COS_node1”, concatenate node1 and node2, 
+#concatenates node1 and node2. If “Score_COS_node2”, concatenates node2 and node1.
+
+combined_interactions_NPAtlas <- combined_interactions_NPAtlas %>%
+  separate(combined_nodes_NPAtlas, into = c("nodeA", "nodeB"), sep = "/")# Now node A takes the position of the node of interest.
+
+
+# Eliminate rows where nodeA and nodeB are the same, i.e. singlets
+combined_interactions_NPAtlas <- combined_interactions_NPAtlas[combined_interactions_NPAtlas$nodeA != combined_interactions_NPAtlas$nodeB, ]
+
+score_COS_NPAtlas <- combined_interactions_NPAtlas %>%
+  select(nodeA, cosine_score) # The Cosine value is filtered according to the node of interest (attached to an annotated node).
+
+# Merge dataframes by id
+BASE_NPAtlas <- merge(MQS_score_NPAtlas, score_COS_NPAtlas, by.x = "id", by.y = "nodeA", all = TRUE) # Note that the MQS value is repeated when there is more than one COS, which is exactly what was sought
+
+
+########################## Retrieving the third parameter, number of unannotated nodes
+score_MSQ_NPAtlas <- df_nodes %>%
+  select(id, MQScore, UniqueFileSources) %>%
+  filter(!is.na(MQScore) & MQScore != "") %>%
+  select(-MQScore)
+
+df_nodes_annotated_NPAtlas <- score_MSQ_NPAtlas %>%
+  separate_rows(UniqueFileSources, sep = "\\|") %>% # Change Sample identifier
+  group_by(UniqueFileSources)
+
+# Count id annotated per sample
+count_nodes_annotated_sample_NPAtlas <- table(df_nodes_annotated_NPAtlas$UniqueFileSources)
+df_count_NPAtlas<- as.data.frame(count_nodes_annotated_sample_NPAtlas)
+
+# total nodes per sample
+nodes_sample_NPAtlas <- df_nodes %>%
+  select(id, UniqueFileSources) %>%
+  separate_rows(UniqueFileSources, sep = "\\|") %>% # Change Sample identifier
+  group_by(UniqueFileSources)
+
+# Count total nodes per sample
+count_nodes_sample_NPAtlas <- table(nodes_sample_NPAtlas$UniqueFileSources)
+df_count_total_nodes_NPAtlas<- as.data.frame(count_nodes_sample_NPAtlas)
+
+# Calculate nodes annotated per sample/ total nodes per sample
+nodes_annotated_total_NPAtlas= merge(df_count_NPAtlas, df_count_total_nodes_NPAtlas, by= "Var1")
+nodes_annotated_total_NPAtlas$Percentage <- (nodes_annotated_total_NPAtlas$Freq.x / nodes_annotated_total_NPAtlas$Freq.y) 
+df_Unn_NPAtlas <- nodes_annotated_total_NPAtlas %>% select(-Freq.x,-Freq.y)
+
+
+######Shannon index for MQS y Cos
+# Verificar si el directorio existe, si no, crearlo
+if (!dir.exists("Data/CSV_IQ_NPAtlas")) {
+  dir.create("Data/CSV_IQ_NPAtlas")
+}
+
+# Separate the samples and extend the dataframe
+df_separated_NPAtlas <- BASE_NPAtlas %>%
+  separate_rows(UniqueFileSources, sep = "\\|") %>%
+  group_by(UniqueFileSources)
+
+# Define strata for MQScore
+MQS_breaks_NPAtlas <- seq(0.01, 1, by = 0.05)
+strata_MQS_NPAtlas <- setNames(
+  lapply(seq_along(MQS_breaks_NPAtlas)[-length(MQS_breaks_NPAtlas)], function(i) c(MQS_breaks_NPAtlas[i], MQS_breaks_NPAtlas[i + 1])),
+  paste0(sprintf("%.2f", MQS_breaks_NPAtlas[-length(MQS_breaks)]), "-", sprintf("%.2f", MQS_breaks_NPAtlas[-1])))
+
+# Define strata for cosine score
+cos_breaks_NPAtlas <- seq(0.70, 1.00, by = 0.05)
+strata_COS_NPAtlas <- setNames(
+  lapply(seq_along(cos_breaks_NPAtlas)[-length(cos_breaks_NPAtlas)], function(i) c(cos_breaks_NPAtlas[i], cos_breaks_NPAtlas[i + 1])),
+  paste0(sprintf("%.2f", cos_breaks_NPAtlas[-length(cos_breaks_NPAtlas)]), "-", sprintf("%.2f", cos_breaks_NPAtlas[-1])))
+
+# Function to stratify and count values, ignoring NAs
+stratify_and_count <- function(df, column, strata) {
+  counts <- sapply(strata, function(range) {
+    sum(df[[column]] >= range[1] & df[[column]] < range[2], na.rm = TRUE)
+  })
+  return(counts)
+}
+
+# Function to stratify and count values, ignoring NAs
+stratify_and_count_unique <- function(df, column, strata) {
+  unique_df <- df %>% 
+    group_by(id) %>% 
+    slice(1) %>%  # Counts only the first occurrence of each unique identifier (id) in the specified column. 
+    ungroup()
+  
+  # count unique values for strata
+  counts <- sapply(strata, function(range) {
+    sum(unique_df[[column]] >= range[1] & unique_df[[column]] < range[2], na.rm = TRUE)
+  })
+  return(counts)
+}
+
+# Function to calculate Shannon Index
+calculate_shannon_index <- function(counts) {
+  proportions <- counts / sum(counts)
+  proportions <- proportions[proportions > 0] # Eliminate proportions 0 to avoid log(0)
+  H <- -sum(proportions * log(proportions))
+  return(H)
+}
+
+# Create lists to save individual df´s and strata counts
+individual_dfs_NPAtlas <- list()
+strata_counts_NPAtlas <- list()
+
+# Create a df´s to save Shannon Index for each sample
+IQ_Summary_NPAtlas <- data.frame(
+  Sample = character(),
+  MQScore_Shannon_Index = numeric(),
+  Cosine_Score_Shannon_Index = numeric(),
+  stringsAsFactors = FALSE
+)
+
+# For each sample, create an individual df and strata counts
+for (sample_name in unique(df_separated_NPAtlas$UniqueFileSources)) {
+  individual_df_NPAtlas <- df_separated_NPAtlas %>%
+    filter(UniqueFileSources == sample_name) %>%
+    select(id, MQScore, cosine_score)
+  
+  # Counting unique values in each stratum for MQScore
+  MQScore_counts_NPAtlas <- stratify_and_count_unique(individual_df_NPAtlas, "MQScore", strata_MQS)
+  
+  # Count values in each stratum for cosine_score
+  cosine_score_counts_NPAtlas <- stratify_and_count(individual_df_NPAtlas, "cosine_score", strata_COS)# have to be not unique
+  
+  # Calculating the Shannon index for MQScore
+  MQScore_shannon_NPAtlas <- calculate_shannon_index(MQScore_counts_NPAtlas)
+  
+  # Calculate Shannon index for cosine_score
+  cosine_score_shannon_NPAtlas <- calculate_shannon_index(cosine_score_counts_NPAtlas)
+  
+  # Ensure that the strata names are consistent.
+  counts_df_NPAtlas <- data.frame(
+    Stratum = names(strata_MQS),
+    MQScore_Count_NPAtlas = MQScore_counts,
+    Cosine_Score_Count_NPAtlas = cosine_score_counts[1:length(MQScore_counts)]  
+  )
+  
+  # Añadir el índice de Shannon como la última fila
+  counts_df_NPAtlas <- rbind(counts_df_NPAtlas, data.frame(
+    Stratum = "Shannon_Index",
+    MQScore_Count_NPAtlas = MQScore_shannon_NPAtlas,
+    Cosine_Score_Count_NPAtlas = cosine_score_shannon_NPAtlas
+  ))
+  
+  # Store count dataframe in the list
+  strata_counts[[sample_name]] <- counts_df_NPAtlas
+  
+  # Assign the individual dataframe to the list
+  individual_dfs_NPAtlas[[sample_name]] <- individual_df_NPAtlas
+  
+  # Adding the Shannon index to the summary dataframe
+  IQ_Summary_NPAtlas <- rbind(IQ_Summary_NPAtlas, data.frame(
+    Sample = sample_name,
+    MQScore_Shannon_Index_NPAtlas = MQScore_shannon_NPAtlas,
+    Cosine_Score_Shannon_Index_NPAtlas = cosine_score_shannon_NPAtlas
+  ))
+}
+
+# Merge the IQ_Summary with df_conunt based on the Sample and Var1 columns
+IQ_Summary_NPAtlas <- merge(IQ_Summary_NPAtlas, df_Unn_NPAtlas, by.x = "Sample", by.y = "Var1", all.x = TRUE)
+
+# Rename columns and reorder them
+colnames(IQ_Summary_NPAtlas) <- c("Sample", "Hmsq", "Hcos", "Ans")
+IQ_Summary_NPAtlas <- IQ_Summary_NPAtlas[, c("Sample", "Hmsq", "Hcos", "Ans")]
+
+# Delete rows with NA in columns Hmsq and Hcos
+IQ_Summary_NPAtlas <- IQ_Summary_NPAtlas[complete.cases(IQ_Summary_NPAtlas$Hmsq, IQ_Summary_NPAtlas$Hcos), ]
+
+
+# min-max normalization
+IQ_Summary_NPAtlas$Hmsq <- (IQ_Summary_NPAtlas$Hmsq - min(IQ_Summary_NPAtlas$Hmsq)) / (max(IQ_Summary_NPAtlas$Hmsq) - min(IQ_Summary_NPAtlas$Hmsq))
+IQ_Summary_NPAtlas$Hcos <- (IQ_Summary_NPAtlas$Hcos - min(IQ_Summary_NPAtlas$Hcos)) / (max(IQ_Summary_NPAtlas$Hcos) - min(IQ_Summary_NPAtlas$Hcos))
+
+
+IQ_Summary_NPAtlas$IQ <- ifelse(is.na(IQ_Summary_NPAtlas$Hmsq), 0, IQ_Summary_NPAtlas$Hmsq) +
+  ifelse(is.na(IQ_Summary_NPAtlas$Hcos), 0, IQ_Summary_NPAtlas$Hcos) +
+  ifelse(is.na(IQ_Summary_NPAtlas$Ans), 0, IQ_Summary_NPAtlas$Ans)
+
+IQ_Summary_NPAtlas$IQ= (3-IQ_Summary_NPAtlas$IQ)/3
+
+# Save each individual dataframe and the strata counts to CSV files in the CSV_IQ folder (Se guardan con un nombre distinto)
+for(sample_name in names(individual_dfs_NPAtlas)) {
+  write.csv(individual_dfs_NPAtlas[[sample_name]], file = paste0("Data/CSV_IQ_NPAtlas/", sample_name, "_data_NPAtlas.csv"), row.names = FALSE)
+  write.csv(strata_counts_NPAtlas[[sample_name]], file = paste0("Data/CSV_IQ_NPAtlas/", sample_name, "_strata_counts_NPAtlas.csv"), row.names = FALSE)
+  
+  # Assign each dataframe to a variable in the R environment
+  assign(sample_name, individual_dfs_NPAtlas[[sample_name]])
+}
+
+# Save the updated Shannon Index summary dataframe to a CSV file in the CSV_IQ folder
+write.csv(IQ_Summary_NPAtlas, file = "Data/CSV_IQ_NPAtlas/1_IQ_Summary_NPAtlas.csv", row.names = FALSE)
+
 
 
 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################################################
-#INTEGRACION DE LOS INDICES POR CEPA
-IQ_for_strain=IQ_Summary_NPAtlas %>% select(-Hmsq, -Hcos, -Nsna)
-
-Metadata_Cruseman <- read_delim("Metadata_Cruseman.csv", 
-                             delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
-
-IQ_NPAtlas_by_strain_w_metadata<- merge(IQ_for_strain, Metadata_Cruseman, by.x = "Sample", by.y = "filename", all = TRUE)
-IQ_NPAtlas_by_strain_w_metadata<- aggregate(IQ ~ ATTRIBUTE_strain, data = IQ_NPAtlas_by_strain_w_metadata, FUN = sum)
-
-Metadata_Cruseman=Metadata_Cruseman %>% select(-filename, -ATTRIBUTE_solvent)
-IQ_NPAtlas_by_strain_w_metadata<- merge(IQ_NPAtlas_by_strain_w_metadata, Metadata_Cruseman, by.x = "ATTRIBUTE_strain", by.y =  "ATTRIBUTE_strain")
-IQ_NPAtlas_by_strain_w_metadata_clean <- IQ_NPAtlas_by_strain_w_metadata %>% distinct()
-
-# Clustering plot
-
-IQ_NPAtlas_by_strain_w_metadata_clean <- na.omit(IQ_NPAtlas_by_strain_w_metadata_clean)
-
-# Escala las variables de interés (IQ)
-IQ_NPAtlas_by_strain_w_metadata_clean_scaled <- scale(IQ_NPAtlas_by_strain_w_metadata_clean$IQ)
-
-# Aplicar k-means con 3 clusters (puedes ajustar el número de clusters)
-set.seed(123)  # Para reproducibilidad
-kmeans_result_NPAtlas_by_strain <- kmeans(IQ_NPAtlas_by_strain_w_metadata_clean_scaled, centers = 5)
-
-# Añadir la columna de clusters al dataframe
-IQ_NPAtlas_by_strain_w_metadata_clean$Cluster <- as.factor(kmeans_result_NPAtlas_by_strain$cluster)
-
-
-library(RColorBrewer)
-
-# Usar una paleta con colores diferenciados
-ggplot(IQ_NPAtlas_by_strain_w_metadata_clean, aes(x = Cluster, y = IQ, color = ATTRIBUTE_Bacteria)) +
-  geom_jitter(size = 3, width = 0.2, height = 0, alpha = 0.8) +  # Jitter en el eje x
-  labs(title = "Visualización de IQ por Clusters",
-       x = "Clusters",
-       y = "IQ",  
-       color = "Bacteria") +
-  theme_minimal() +
-  theme(axis.text.x = element_blank(),
-        axis.ticks.x = element_blank()) +
-  scale_color_brewer(palette = "Set1")  # Usar paleta Set1 de RColorBrewer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-top_10_bajos <- IQ_for_strain %>% 
-  arrange(IQ) %>%          
-  slice_head(n = 10)
-
-# Gráfico de puntos de IQ por ATTRIBUTE_strain
-ggplot(top_10_bajos, aes(x = ATTRIBUTE_strain, y = IQ)) +
-  geom_point(color = "blue", size = 3) +
-  labs(title = "Top 10 lowest IQ by strain",
-       x = "Strain (ATTRIBUTE_strain)",
-       y = "IQ") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotar etiquetas del eje x
-
-
-
-
-
-
