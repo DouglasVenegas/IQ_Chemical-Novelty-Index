016a0a2aa56c92544947cfab1e7829f342a89c7f
Douglas Venegas <douglas.venegas@ucr.ac.cr>
2025-01-07 09:15:08 -0600

    Add files via upload

---
+++ Install.R
@@ -0,0 +1,18 @@
+if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
+pacman::p_load(
+  RColorBrewer, 
+  tidyverse, 
+  readxl, 
+  rvest, 
+  dplyr, 
+  tidyr, 
+  igraph, 
+  visNetwork, 
+  readr, 
+  ggplot2, 
+  ggraph, 
+  graphTweets, 
+  writexl, 
+  httr, 
+  proxy
+)
