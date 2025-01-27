bbe16d3a68f615dbc52bc2585f4f38eec5d6c778
Douglas Venegas <douglas.venegas@ucr.ac.cr>
2025-01-27 16:24:39 -0600

    Update Install.R

--- Install.R
+++ Install.R
@@ -1,9 +1,1 @@
-install.packages(c(
-  "repr", "IRdisplay", "evaluate", "crayon", "pbdZMQ", "devtools", "uuid", 
-  "digest", "RColorBrewer", "tidyverse", "readxl", "rvest", "dplyr", 
-  "tidyr", "igraph", "visNetwork", "readr", "ggplot2", "ggraph", 
-  "graphTweets", "writexl", "httr", "proxy"
-))
-install.packages("devtools")
-devtools::install_github("IRkernel/IRkernel")
-IRkernel::installspec(user = FALSE)
+install.packages("igraph")
