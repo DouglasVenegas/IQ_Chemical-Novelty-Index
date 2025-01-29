fa31ff4c34a7ef1bd6e23eaf79239d9934aa3e84
Douglas Venegas <douglas.venegas@ucr.ac.cr>
2025-01-29 10:42:27 -0600

    Update Install.R

--- Install.R
+++ Install.R
@@ -1,2 +1,1 @@
-install.packages(c("remotes","readr")
-remotes::install_version("igraph", version = "2.1.4", repos = "https://cran.r-project.org")
+if (!requireNamespace("readr", quietly = TRUE)) install.packages("readr")
